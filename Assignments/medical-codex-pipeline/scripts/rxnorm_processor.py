c8abdfae14a8371082a2df16fd916b317d1ba50c
yolo <224895660+y0y0l0@users.noreply.github.com>
2025-09-07 22:41:07 -0400

    need to revisit icd10cm script and rxnorm updates

--- Assignments/medical-codex-pipeline/scripts/rxnorm_processor.py
+++ Assignments/medical-codex-pipeline/scripts/rxnorm_processor.py
@@ -1,11 +1,10 @@
 import polars as pl
+import pandas as pd
 from pathlib import Path
 from utils.common_functions import normalizeColumnNames, save_to_formats
-fileInputPath= 'input/ICD_10_CM_US/icd10cm_order_2026.txt'
-fileOutputPath ='output'
 
-# https://www.nlm.nih.gov/research/umls/rxnorm/docs/techdoc.html
-file_path = Path('Module1_MedicalCodexes/rxnorm/files/RXNATOMARCHIVE.RRF')
+fileInputPath= 'input/RxNorm/RXNATOMARCHIVE.RRF'
+fileOutputPath ='output'
 
 columns = [
     'rxaui', 'aui', 'str', 'archive_timestamp', 'created_timestamp', 
@@ -13,23 +12,16 @@ columns = [
     'saui', 'vsab', 'rxcui', 'sab', 'tty', 'merged_to_rxcui'
 ]
 
-df = pl.read_csv(
-    file_path,
+raw_data= pl.read_csv(
+    fileInputPath,
     separator='|',
     has_header=False,
     new_columns=columns,
     truncate_ragged_lines=True
 )
-
-output_dir = Path('Module1_MedicalCodexes/rxnorm/output')
-output_dir.mkdir(exist_ok=True)
-output_path = output_dir / 'RXNATOMARCHIVE.csv'
-
-df.write_csv(output_path)
-
-print(f"Successfully parsed {len(df)} records from RXNATOMARCHIVE.RRF")
-print(f"Saved to {output_path}")
-print(f"Dataset shape: {df.shape}")
-print(f"\nFirst 5 rows:")
-print(df.head())
-print(f"\nMemory usage (MB): {df.estimated_size() / 1024**2:.2f}")
+cols = ['code', 'str', 'rxaui','sab','last_released']
+#clean data
+clean_data = raw_data [cols].to_pandas()
+clean_data=normalizeColumnNames(clean_data,pd)
+## save cleaned data to output path
+save_to_formats(clean_data, fileInputPath,fileOutputPath, "rxnorm2025_processed.csv")
